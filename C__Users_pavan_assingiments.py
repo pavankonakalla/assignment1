0eea893d43875b39a05a32dc6ebe5c6d32d0b4ee
rajee12 <86417245+rajee12@users.noreply.github.com>
2021-06-25 20:30:01 +0530

    Add files via upload

---
+++ C__Users_pavan_assingiments.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+b=[]
+for i in range(2000,3200):
+    if i%7==0 and 1%7!=0:
+        b.append(i)
+b
+
+
+# In[6]:
+
+
+a=input("enetr a fname")
+b=input("enter sname")
+print(b+' '+ a)
+
+
+# In[8]:
+
+
+pi=22/7
+d=12
+v=4/3*22/7*(d/2)**3
+print(v)
+
+
+# In[ ]:
+
+
+
+
